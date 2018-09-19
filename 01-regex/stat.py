5e6fd1741085b3eb46c1ea11cd17706ddf44c6e4
Tomas Ritter <xritter@nymfe84.fi.muni.cz>
2018-09-19 16:45:25 +0200

    WIP

--- 01-regex/stat.py
+++ 01-regex/stat.py
@@ -1,21 +1,28 @@
 import sys
+import re
 
-to_match = "";
+to_match = ""
 if sys.argv[2] == "composer":
     to_match = "Composer:"
-else if sys.argv[2] = "century":
+elif sys.argv[2] == "century":
     to_match = "Publication Year:" 
-else if sys.argv[2] = "cminor":
-    to_match = "Key:"
+elif sys.argv[2] == "cminor":
+    to_match = "Key: (.*)c"
+else:
+    exit
 
 dict = {}
 for line in open(sys.argv[1], 'r'):
-    r = re.compile(r"(.*):(.*)\n");
+    r = re.compile(r"(.*):(.*)");
     m = r.match(to_match)
     if m is None:
         continue
-    if not key in dict:
-        dict[key] = 1
-    else:
-        dict[key] += 1
-    
+    s = r.split(r";")
+    for key in s:
+        if not key in dict:
+            dict[key] = 1
+        else:
+            dict[key] += 1
+for k, v in dict:
+    print("Key: " + k + " Value: "+ v)
+
