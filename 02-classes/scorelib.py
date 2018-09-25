d1ce0da5cfde976c46b60339ceae1837af65a6df
Tomas Ritter <xritter@nymfe35.fi.muni.cz>
2018-09-25 16:49:42 +0200

    Second exercise, not implemented fully yet

--- 02-classes/scorelib.py
+++ 02-classes/scorelib.py
@@ -1,1 +1,33 @@
-a
+class Print:
+    def __init__(self, edition, print_id, partiture):
+        self.edition = edition
+        self.print_id = print_id
+        self.partiture = partiture
+
+class Edition:
+    def __init__(self, composition, authors, name):
+        self.composition = composition
+        self.authors = authors
+        self.name = name
+
+class Composition:
+    def __init__(self, incipit, key, genre, year, voices, authors):
+        self.incipit = incipit
+        self.key = key
+        self.genre = genre
+        self.year = year
+        self.voices = voices
+        self.authors = authors
+
+class Voice:
+    def __init__(self, name, range):
+        self.name = name
+        self.range = range
+
+class Person:
+    def __init__(self, name, born, died):
+        self.name = name
+        self.born = born
+        self.died = died
+
+
--- 02-classes/test.py
+++ 02-classes/test.py
@@ -1,1 +1,24 @@
-a
+import sys
+import re
+
+def get_print(dict):
+    
+    return print;
+
+def load(filename):
+    r = re.compile(r"(.*): (.*)")
+    prints = []
+    data = {}
+    for line in open(sys.argv[1], 'r'):
+        if line == "":
+            prints.append(get_print(dict))
+        m = r.match(line)
+        data[m.group(1)] = m.group(2)
+
+    return prints
+
+
+prints = load(sys.argv[1])
+
+for p in prints:
+    print("VALUE")
