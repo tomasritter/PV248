69f0a3630f4409dc0f3e775a43c3224a244f27aa
Tomas Ritter <xritter@nymfe80.fi.muni.cz>
2018-09-27 11:49:03 +0200

    Added some stuff to 02

--- 02-classes/test.py
+++ 02-classes/test.py
@@ -1,19 +1,28 @@
 import sys
 import re
 
+def get_authors(dict)
+    authors = []
+    return authors
+
+def get_
+
 def get_print(dict):
-    
+    printId = dict["Print Number"]
+    print
     return print;
 
 def load(filename):
-    r = re.compile(r"(.*): (.*)")
+    r = re.compile(r"(.*?): (.*)")
     prints = []
     data = {}
     for line in open(sys.argv[1], 'r'):
         if line == "":
             prints.append(get_print(dict))
+            dict = {}
+            continue
         m = r.match(line)
-        data[m.group(1)] = m.group(2)
+        dict[m.group(1)] = m.group(2) or None
 
     return prints
 
