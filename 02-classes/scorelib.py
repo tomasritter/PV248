bd634ffba62baf7ca50b2fd365db8ff926ebb4ff
Tomas Ritter <tomas.ritter1@gmail.com>
2018-10-08 11:22:58 +0200

    02: moved function to scorelib and format now prints out the output string

--- 02-classes/scorelib.py
+++ 02-classes/scorelib.py
@@ -1,3 +1,6 @@
+import re
+import sys
+
 class Print:
     def __init__(self, edition, print_id, partiture):
         self.edition = edition
@@ -47,7 +50,7 @@ class Print:
             s += "\n"
         s += "Partiture: " + ("yes" if self.partiture else "no") + "\n"
         s += "Incipit: " + (self.edition.composition.incipit if not self.edition.composition.incipit is None else "") + "\n"
-        return s
+        print(s)
         
     def composition(self):
         return self.edition.composition
@@ -79,4 +82,118 @@ class Person:
         self.born = born
         self.died = died
 
+def get_authors(composers):
+    authors = []
+    if composers is None:
+        return authors
+    s = re.split(r"; ", composers)
+    for a in s:
+        # If there is not a "(" symbol, there is no year of birth/death
+        if a.find("(") == -1:
+            authors.append(Person(a, None, None))
+        else:
+            # Match name an everything in the brackets
+            m = re.match(r"(.*) \((.*)\)", a)
+            name = m.group(1)
+            # Try to match numbers around "--" symbols
+            m1 = re.match(r"(\d*)--?(\d*)", m.group(2))
+            if m1 is None:
+                authors.append(Person(name, None, None))
+                continue
+            # Use matched numbers only if there are 4 digits
+            born = int(m1.group(1)) if len(m1.group(1)) == 4 else None
+            died = int(m1.group(2)) if len(m1.group(2)) == 4 else None
+            authors.append(Person(name, born, died))
+    return authors
 
+def get_voices(dict):
+    voices = []
+    k = 1
+    while True:
+        # Go through all the listed voices
+        if ("Voice " + str(k)) in dict:
+            v = dict["Voice " + str(k)]
+            # Empty case
+            if v is None or v == "": 
+                voices.append(Voice(None, None))
+            # If there is range, add id
+            elif "--" in v:
+                # Some lines of Voice were seperated by ";", instead use ","
+                v = v.replace('; ', ', ')
+                m = v.split(", ", 1)
+                voices.append(Voice(m[1] if len(m) != 1 else None, m[0]))
+            else:
+                voices.append(Voice(v or None, None))
+            # Increase the number of the voice
+            k += 1
+        else: 
+            break
+    return voices
+
+def get_editors(editors_string):
+    editors = []
+    if editors_string is None:
+        return editors
+    # Strip ',' and ' ' symbols from the right 
+    editors_string = editors_string.rstrip(", ")
+    # Split on comma
+    s = re.split(r", ", editors_string)
+    skip = False
+    for i in range(len(s)):
+        # Skip only when else statement has executed in the iteration before
+        if skip:
+            skip = False
+            continue
+        # If split contains ' ', it's a full name an will be used as such
+        if s[i].find(" ") != -1:
+            editors.append(Person(s[i], None, None))
+        else:
+            # If split doesn't contain ' ', then try to get next split as part of the name
+            editors.append(Person(s[i] + ((", " + s[i + 1]) if i + 1 < len(s) else ""), None, None))
+            skip = True
+    return editors
+
+
+def get_year(comp_year):
+    if comp_year is None:
+        return None
+    m = re.match(r"\d\d\d\d", comp_year)
+    return m.group(0) if m is not None else None
+
+def get_print(dict):
+    printId = int(dict["Print Number"])
+    authors = get_authors(dict["Composer"])
+    title = dict["Title"] if "Title" in dict else None
+    incipit = dict["Incipit"] if "Incipit" in dict else None
+    key = None
+    if "Key" in dict:
+        key = None if dict["Key"] is "" else dict["Key"]
+    genre = None
+    if "Genre" in dict:
+        genre = None if dict["Genre"] is "" else dict["Genre"]
+    partiture = False
+    if "Partiture" in dict and not dict["Partiture"] is None and "yes" in dict["Partiture"]:
+            partiture = True
+    name = dict["Edition"] or None
+    voices = get_voices(dict)
+    editors = get_editors(dict["Editor"]) if "Editor" in dict else []
+    year = get_year(dict["Composition Year"]) if "Composition Year" in dict else None
+    composition = Composition(title, incipit, key, genre, year, voices, authors)
+    edition = Edition(composition, editors, name)
+    p = Print(edition, printId, partiture)
+    return p;
+
+def load(filename):
+    r = re.compile(r"(.*)?: ?(.*)")
+    prints = []
+    dict = {}
+    for line in open(sys.argv[1], 'r'):
+        m = r.match(line)
+        if m is None:
+            continue
+        dict[m.group(1)] = m.group(2) or None
+        # If incipit has been parsed, we know we are at the endo of a "block"
+        if m.group(1) == "Incipit":
+            prints.append(get_print(dict))
+            dict = {}
+    return prints
