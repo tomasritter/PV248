b00816c8e2e0e63317d6643fa445da5e6560a843
Tomas Ritter <xritter@nymfe35.fi.muni.cz>
2018-10-09 16:50:28 +0200

    New files

--- 03-database/import.py
+++ 03-database/import.py
@@ -1,1 +1,12 @@
-a
+import sqlite3
+import sys
+
+
+conn = sqlite3.connect(sys.argv[2])
+cur = conn.cursor()
+
+conn.execute("create table person ( id integer primary key not null, born integer, died integer, name varchar not null )")
+conn.execute("INSERT INTO table person VALUES (?, ?, ?, ?)", (1, 1954, 1956, "Wolfgang amadeus"))
+conn.commit()
+conn.close()
+
