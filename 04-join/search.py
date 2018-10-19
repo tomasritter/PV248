9e22b6fec88ac4d2d69256b0dddf14bd408bea45
Tomas Ritter <tomas.ritter1@gmail.com>
2018-10-19 22:12:38 +0200

    04: Refactor

--- 04-join/search.py
+++ 04-join/search.py
@@ -9,59 +9,54 @@ def dict_factory(cursor, row):
             d[col[0]] = row[idx]
     return d
 
+def add_value_to_dict(dict, key, value):
+    if not value is None:
+        dict[key] = value
+        
+def add_values_to_dict_list(dict, key, values, cur):
+    dict[key] = []
+    for v in values:
+        dict[key].append(dict_factory(cur, v))
+        
+
 def get_composer_info(cur, row):
     d = {}
     d["Print Number"] = row[1]
-    
-    d["Composer"] = []
+
     composers = cur.execute("SELECT name, born, died FROM score_author INNER JOIN person ON \
                             score_author.composer = person.id WHERE score_author.score = (?)", (row[2],))
-    for c in composers:
-        d["Composer"].append(dict_factory(cur, c))
+    add_values_to_dict_list(d, "Composer", composers, cur)
     
-    if not row[4] is None:
-        d["Title"] = row[4]
+    add_value_to_dict(d, "Title", row[4])
+
+    add_value_to_dict(d, "Genre", row[5])
+
+    add_value_to_dict(d, "Key", row[6])
+        
+    add_value_to_dict(d, "Composition Year", row[7])
     
-    if not row[5] is None:
-        d["Genre"] = row[5]
-        
-    if not row[6] is None:
-        d["Key"] = row[6]
-        
-    if not row[7] is None:
-        d["Composition Year"] = row[7]
-        
-    if not row[8] is None:
-        d["Edition"] = row[8]
+    add_value_to_dict(d, "Edition", row[8])
         
     editors = cur.execute("SELECT name, born, died FROM edition_author INNER JOIN person ON \
                             edition_author.editor = person.id WHERE edition_author.edition = (?)", (row[3],))
-    d["Editor"] = []
-    for e in editors:
-        d["Editor"].append(dict_factory(cur, e))
+    add_values_to_dict_list(d, "Editor", editors, cur)
         
     voices = cur.execute("SELECT range, voice.name FROM voice INNER JOIN score ON \
                             voice.score = score.id WHERE score.id = (?) ORDER BY number", (row[2],))
-    d["Voices"] = []
-    for v in voices:
-        d["Voices"].append(dict_factory(cur, v))
+    add_values_to_dict_list(d, "Voices", voices, cur)
     
-    if not row[9] is None:
-        d["Partiture"] = row[9]
-        
-    if not row[10] is None:
-        d["Incipit"] = row[10]
+    add_value_to_dict(d, "Partiture", row[9])
+     
+    add_value_to_dict(d, "Incipit", row[10])
     return d
 
 conn = sqlite3.connect("scorelib.dat")
 cur = conn.cursor()
 
-name = sys.argv[1]
-query = "%" + name + "%"
-cur.execute("SELECT person.name, print.id, score.id, edition.id, score.name, score.genre, score.key, score.year, \
-            edition.name, partiture, incipit FROM \
-            print INNER JOIN edition ON print.edition = edition.id \
-            INNER JOIN score ON \
+query = "%" + sys.argv[1] + "%"
+cur.execute("SELECT person.name, print.id, score.id, edition.id, score.name, \
+            score.genre, score.key, score.year, edition.name, partiture, incipit FROM \
+            print INNER JOIN edition ON print.edition = edition.id INNER JOIN score ON \
             edition.score = score.id INNER JOIN score_author ON score.id = score_author.score \
             INNER JOIN person ON score_author.composer = person.id WHERE person.name like (?)", (query,))
 
